1e5a3b87b2141c252a1c0d6b80b6a81f3bd5c07b
IKSEONG <devikseong@gmail.com>
2024-04-09 09:54:54 +0900

    init

---
+++ app/src/main/java/com/devikseong/composeui/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.devikseong.composeui
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.telephony.SmsManager
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import com.devikseong.composeui.ui.theme.ComposeUITheme
+
+
+class MainActivity : ComponentActivity() {
+
+    private val PERMISSION_SEND_SMS = 123
+
+    private fun checkForSmsPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), PERMISSION_SEND_SMS)
+        } else {
+            // 권한이 이미 있을 경우, SMS를 바로 보낼 수 있음
+        }
+    }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_SEND_SMS && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // 권한이 부여되었을 때의 처리
+        }
+    }
+
+    companion object {
+        private const val SMS_PERMISSION_REQUEST = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        checkForSmsPermission()
+
+        setContent {
+            ComposeUITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SmsScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeUITheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun SmsScreen() {
+    val context = LocalContext.current
+    var phoneNumbers by remember { mutableStateOf("") }
+    var message by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        OutlinedTextField(
+            value = phoneNumbers,
+            onValueChange = { phoneNumbers = it },
+            label = { Text("전화번호 (콤마로 구분)") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = message,
+            onValueChange = { message = it },
+            label = { Text("메시지 내용") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                val phoneNumberList = phoneNumbers.split(",").map { it.trim() }
+                val smsManager = SmsManager.getDefault()
+                for (number in phoneNumberList) {
+                    smsManager.sendTextMessage(number, null, message, null, null)
+                }
+
+//                val smsIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:01099039815;01021089815"))
+//                smsIntent.putExtra("sms_body", "sms message goes here")
+//                startActivity(context, smsIntent, null)
+
+                Toast.makeText(context, "문자가 전송되었습니다.", Toast.LENGTH_SHORT).show()
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text("전송")
+        }
+    }
+}
